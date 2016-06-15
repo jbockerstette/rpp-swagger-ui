79eaf45ac82423eb87c67abe7957723ed52e28a8
jbockerstette <jim.bockerstette@gmail.com>
2016-06-14 23:20:59 -0400

    Modified the fix-ip script to handle finding the old ip via regex and replacing it with the new one.

--- dist/fix-ip-in-index.sh
+++ dist/fix-ip-in-index.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script will do a find and replace in the file index.html and replace all instances
-# of the ip address of 162.243.36.108 with the ip address of this machine. This is necessary
+# of the ip address of xxx.xxx.xxx.xxx with the ip address of this machine. This is necessary
 # so that the swagger ui will work properly.
 #
 #Get the ip address of this machine.
 IP=$(hostname -I | cut -f1 -d' ')
 #echo $IP
 #Use perl to do a find and replace of the old ip address with the actual one on this box.
-perl -pi -e "s/162.243.36.108/$IP/g" /root/swagger-ui/dist/index.html
+perl -pi -e "s/((\d{1,3}\.){3})\d+/$IP/g" /root/swagger-ui/dist/index.html
